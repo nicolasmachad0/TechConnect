8d32444d31d5d30fb58baef239d977f920c0e0f1
Nicolas Machado Fogaça <124478422+nicolasmachad0@users.noreply.github.com>
2026-05-24 19:03:21 -0300

    Adicionar arquivos de projeto.

---
+++ TechConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
