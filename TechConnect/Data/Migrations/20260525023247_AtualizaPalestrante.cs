b0b6a99365373630122b98426d5a21361e6b3245
Nicolas Machado Fogaça <124478422+nicolasmachad0@users.noreply.github.com>
2026-05-25 00:00:29 -0300

    Controle de edição do Administrador e usuario comum

---
+++ TechConnect/Data/Migrations/20260525023247_AtualizaPalestrante.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechConnect.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizaPalestrante : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cargo",
+                table: "Palestrante",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cargo",
+                table: "Palestrante");
+        }
+    }
+}
---
+++ TechConnect/Data/Migrations/20260525023813_Palestrante.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechConnect.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Palestrante : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Cargo",
+                table: "Palestrante",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Cargo",
+                table: "Palestrante",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
